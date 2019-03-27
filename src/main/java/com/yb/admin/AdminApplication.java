7793be6121f65b4fd261e7f7426d1408e96648bf
yangbiao <itbiaoyang@163.com>
2019-03-27 15:07:36 +0800

    a

---
+++ src/main/java/com/yb/admin/AdminApplication.java
@@ -0,0 +1,22 @@
+package com.yb.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@EnableAdminServer
+@SpringCloudApplication//已经包含@EnableDiscoveryClient
+public class AdminApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(AdminApplication.class);
+    }
+
+}
---
+++ src/main/java/com/yb/admin/config/SecuritySecureConfig.java
@@ -0,0 +1,40 @@
+package com.yb.admin.config;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+/**
+ * Description:注意环境,只有配置文件里指定了正确的环境,实测才能进入正常的那个admin的UI登录页面
+ * 下面的这个配置是admin的官方文档copy来的,实测用了这个配置就可以完成对应的功能了
+ * author biaoyang
+ * date 2019/3/27 002714:55
+ */
+@Profile({"dev", "test"})
+@Configuration
+public class SecuritySecureConfig extends WebSecurityConfigurerAdapter {
+    private final String adminContextPath;
+
+    public SecuritySecureConfig(AdminServerProperties adminServerProperties) {
+        this.adminContextPath = adminServerProperties.getContextPath();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        SavedRequestAwareAuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter("redirectTo");
+
+        http.authorizeRequests()
+                .antMatchers(adminContextPath + "/assets/**").permitAll()
+                .antMatchers(adminContextPath + "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage(adminContextPath + "/login").successHandler(successHandler).and()
+                .logout().logoutUrl(adminContextPath + "/logout").and()
+                .httpBasic().and()
+                .csrf().disable();
+    }
+}
